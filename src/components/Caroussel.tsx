e5a20bdd5f24cbf14770bd4c6c17fafbed8c7763
Enrique Comesaña <enriquecbar@gmail.com>
2024-03-29 21:05:12 +0100

    footer

--- src/components/Caroussel.tsx
+++ src/components/Caroussel.tsx
@@ -4,14 +4,14 @@ import { BsArrowLeftSquareFill, BsArrowRightSquareFill } from 'react-icons/bs';
 
 const Caroussel = () => {
 
-    const [activeSlideIndex, setActiveSlideIndex] = useState(0);
+    const [activeSlide, setactiveSlide] = useState(0);
     const length = sliderData.length;
 
     const prevSlide = () => {
-        setActiveSlideIndex(activeSlideIndex === length - 1 ? 0 : activeSlideIndex + 1);
+        setactiveSlide(activeSlide === length - 1 ? 0 : activeSlide + 1);
       };
       const nextSlide = () => {
-        setActiveSlideIndex(activeSlideIndex === 0 ? length - 1 : activeSlideIndex - 1);
+        setactiveSlide(activeSlide === 0 ? length - 1 : activeSlide - 1);
       };
   
   return( 
@@ -20,8 +20,8 @@ const Caroussel = () => {
       <BsArrowLeftSquareFill className='absolute top-[50%] text-3xl text-white cursor-pointer left-8'onClick={prevSlide}/>
       <BsArrowRightSquareFill className='absolute top-[50%] text-3xl text-white cursor-pointer right-8'onClick={nextSlide}/>
 {sliderData.map((slide, index) => (
-    <div className={index === activeSlideIndex ? "opacity-100" : "opacity-0"} key={index}>
-       {index ===activeSlideIndex && <img src={slide.url} alt="/" className='w-full rounded-md'/>}
+    <div className={index === activeSlide ? "opacity-100" : "opacity-0"} key={index}>
+       {index ===activeSlide && <img src={slide.url} alt="/" className='w-full rounded-md'/>}
     </div>
 ))}
 
